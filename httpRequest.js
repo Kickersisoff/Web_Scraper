b3e7e1815fb01370880addb9a404d1b52371b751
Kickersisoff <34878344+Kickersisoff@users.noreply.github.com>
2023-02-11 23:04:51 +0530

    Update httpRequest.js

--- httpRequest.js
+++ httpRequest.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 async function amazonHtml(amazonUrl) {
     const { data: html } = await axios.get(amazonUrl, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/'
       }
     })
     .catch(function (error) {
